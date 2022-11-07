e350a76424a781b1349e67e26ea6b0ab32174b56
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-11-07 21:44:08 +0100

    currency picker list reloaction

--- src/Components/Actions/Actions.js
+++ src/Components/Actions/Actions.js
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import Cart from "../Cart/Cart";
 import CurrencyPicker from "../CurrencyPicker/CurrencyPicker";
-import OverlayWrapper from "../OverlayWrapper/OverlayWrapper";
 
 class Actions extends Component {
 
@@ -14,38 +13,12 @@ class Actions extends Component {
         }
     }
 
-    handleCurrenciesListOpen(){
-        if (this.state.isCurrenciesListOpen) {
-            this.setState({isCurrenciesListOpen: false});
-        } else {
-            this.setState({isCurrenciesListOpen: true});
-        }
-    }
-
-    handleCurrencyChange(e){
-        const currencyOptionSymbol = e.target.children[0].textContent;
-        this.setState({currentCurrencySymbol: currencyOptionSymbol})
-    }
-
     render() {
         return (
             <div className="actions">
                 <CurrencyPicker/>
                 <Cart/>
-                { this.state.isOverlayWrapperOpen && 
-                    <OverlayWrapper component={<CurrencyPicker 
-                        state={this.state} 
-                        handleCurrenciesListOpen={this.handleCurrenciesListOpen.bind(this)}
-                    />} />
-                }
-                { this.state.isOverlayWrapperOpen && 
-                    <OverlayWrapper>
-                        <CurrencyPicker 
-                            state={this.state} 
-                            handleCurrenciesListOpen={this.handleCurrenciesListOpen.bind(this)}
-                        />
-                    </OverlayWrapper>}
-                }
+
             </div>
         )
     }
--- src/Components/CurrencyPicker/CurrencyPicker.js
+++ src/Components/CurrencyPicker/CurrencyPicker.js
@@ -1,32 +1,90 @@
+import { gql } from "@apollo/client";
 import { Component } from "react";
+import { Query } from "@apollo/client/react/components";
 import {BiChevronDown as CurrencyPickerDash} from 'react-icons/bi';
+
+const currenciesArray = gql`
+    {
+        currencies {
+            label,
+            symbol
+        }
+    }
+`
 
 class CurrencyPicker extends Component {
 
-    constructor(props){
-        super();
-        this.currentCurrencySymbol = props.state.currentCurrencySymbol;
-        this.isCurrenciesListOpen = props.state.isCurrenciesListOpen;
-        this.handleCurrenciesListOpen = props.handleCurrenciesListOpen;
+    constructor() {
+        super()
+        this.state = {
+            isCurrenciesListOpen: false,
+            currentCurrencySymbol: "$"
+        }
     }
 
+    handleCurrenciesListOpen(){
+        if (this.state.isCurrenciesListOpen) {
+            this.setState({isCurrenciesListOpen: false});
+        } else {
+            this.setState({isCurrenciesListOpen: true});
+        }
+    }
+
+    handleCurrencyChange(e){
+        const currencyOptionSymbol = e.target.children[0].textContent;
+        this.setState({currentCurrencySymbol: currencyOptionSymbol})
+    }
 
     render() {
-        console.log("XD")
         return (
             <div className="currency-picker">
 
                 <div className="show-currencies-container">
 
-                    <label htmlFor="show-currencies">{this.currentCurrencySymbol}</label>
+                    <label htmlFor="show-currencies">{this.state.currentCurrencySymbol}</label>
                     <button 
                         onClick={this.handleCurrenciesListOpen.bind(this)} 
-                        className={`show-currencies ${this.isCurrenciesListOpen ? "dash-open" : ""} `}
+                        className={`show-currencies ${this.state.isCurrenciesListOpen ? "dash-open" : ""} `}
                     >
                         <CurrencyPickerDash/>
                     </button>
 
                 </div>
+                {
+                    this.state.isCurrenciesListOpen &&
+                    <Query query={currenciesArray}>
+                        {({loading, data}) => {
+                
+                            if (loading) return "Loading...";
+                
+                            const {currencies} = data;
+                
+                            const currencyList = currencies.map(currency => {
+                                const {label, symbol} = currency;
+
+                                return (
+                                    
+                                    <button
+                                        key={label} 
+                                        className="currency-option"
+                                        onClick={this.handleCurrencyChange.bind(this)}
+                                    >
+                                        <span className="currency-symbol">{symbol}</span>
+                                        <span className="currency-label">{label}</span>
+                                    </button>
+                                )
+                            })
+                
+                            return (
+                                <div 
+                                    className={`currencies-options-container ${this.state.isCurrenciesListOpen ? "currencies-options-container-visible" : ""} `}
+                                >
+                                    {currencyList}
+                                </div> 
+                            )
+                        }}
+                    </Query>
+                }
 
             </div>
         )
