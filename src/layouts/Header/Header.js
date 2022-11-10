09e1156734e1f99007d68814b11ba146ccfea719
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-11-10 19:16:29 +0100

    fixed overflowing background

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import Actions from "../../Components/Actions/Actions";
 import Logo from "../../Components/Logo/Logo";
-import Categories from "../../Components/Navigation/Navigation";
+import Navigation from "../../Components/Navigation/Navigation";
 import './Header.scss';
 
 class Header extends Component {
@@ -9,7 +9,7 @@ class Header extends Component {
     render(){
         return (
             <header>         
-                <Categories/> 
+                <Navigation/> 
                 <Logo/>
                 <Actions handleCartOverlayVisibleToggle={this.props.handleCartOverlayVisibleToggle}/>
             </header>
