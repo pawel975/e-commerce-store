4728e6dac03a5560da6975a5eeb95231cca29b84
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-11-11 17:45:06 +0100

    switching category on category products page

--- src/Components/CategoryProducts/CategoryProducts.js
+++ src/Components/CategoryProducts/CategoryProducts.js
@@ -1,12 +1,78 @@
+import { gql } from "@apollo/client";
+import { Query } from "@apollo/client/react/components";
 import { Component } from "react";
 import capitalizeWord from "../../helpers/capitalizeWord";
 import "./CategoryProducts.scss";
 
 class CategoryProducts extends Component {
+
+    queryProducts(category){
+        return gql`
+        {
+            category(input: { title: "${category}" }) {
+                name
+                products {
+                    id
+                    name
+                    inStock
+                    gallery
+                    description
+                    category
+                    attributes {
+                        id
+                        name
+                        type
+                        items {
+                            displayValue
+                            value
+                            id
+                        }
+                    }
+                    prices {
+                        currency {
+                            label
+                            symbol
+                        }
+                        amount
+                    }
+                    brand
+                }
+            }
+        }
+    `;
+    }
+
     render(){
         return (
             <section className="category-products">
-                <h2 className="category-products__category-name">{capitalizeWord(this.props.currentCategory)}</h2>
+
+                <h2 className="category-products__category-name">
+                    {capitalizeWord(this.props.currentCategory)}
+                </h2>
+
+                <div className="category-products__products-list">
+
+                    <Query query={this.queryProducts(this.props.currentCategory)}>
+                        {({loading, data}) => {
+                
+                            if (loading) return "Loading...";
+                
+                            const {products} = data.category;
+                
+                            const productsList = products.map(product => {
+                                const {name} = product;
+                
+                                return (
+                                    <span key={name}>{name}</span>
+                                )
+                            })
+                
+                            return <div>{productsList}</div>
+                        }}
+                    </Query>
+                    
+                </div>
+
             </section>
         )
     }
